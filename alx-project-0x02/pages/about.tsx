f56095c64c674888ef0b8a7769758367a9f012a0
Brianryanphiri <105008060+Brianryanphiri@users.noreply.github.com>
2025-07-22 09:44:13 +0000

    Add Header component to About page

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,16 +1,20 @@
-import type { NextPage } from 'next'
-import Button from '@/components/common/Button'
+import type { NextPage } from 'next';
+import Button from '@/components/common/Button';
+import Header from '@/components/layout/Header';
 
 const About: NextPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen gap-4 bg-yellow-100 p-4">
-      <h1 className="text-4xl font-bold text-yellow-700 mb-6">This is the About Page</h1>
+    <>
+      <Header />
+      <div className="flex flex-col items-center justify-center min-h-screen gap-4 bg-yellow-100 p-4">
+        <h1 className="text-4xl font-bold text-yellow-700 mb-6">This is the About Page</h1>
 
-      <Button label="Small Button" size="small" shape="rounded-sm" />
-      <Button label="Medium Button" size="medium" shape="rounded-md" />
-      <Button label="Large Button" size="large" shape="rounded-full" />
-    </div>
-  )
-}
+        <Button label="Small Button" size="small" shape="rounded-sm" />
+        <Button label="Medium Button" size="medium" shape="rounded-md" />
+        <Button label="Large Button" size="large" shape="rounded-full" />
+      </div>
+    </>
+  );
+};
 
-export default About
+export default About;
